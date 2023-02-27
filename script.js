a7b4b053d04e43457a5d7cd9b8788335e0ecd1e7
Kshitij adhikaree <adhikareekhsitij@gmail.com>
2023-02-27 18:36:15 +0545

    add loader in js

--- script.js
+++ script.js
@@ -3,11 +3,24 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const Loader = document.getElementById("loader");
 
 let apiQuotes = [];
 
+//Show Loading
+function loading() {
+  Loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide loading
+function complete() {
+  quoteContainer.hidden = false;
+  Loader.hidden = true;
+}
 //Show New Quote
 function newQuote() {
+    loading()
   //Pick a random quote from apiQuotes array
 
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
@@ -25,12 +38,14 @@ function newQuote() {
   } else {
     quoteText.classList.remove("long-quote");
   }
-
+// Set Quote, Hide Loader
   quoteText.textContent = quote.text;
+  complete();
 }
 
 // Get Quotes form API
 async function getQuotes() {
+    loading()
   const apiUrl = "https://type.fit/api/quotes";
   try {
     const response = await fetch(apiUrl);
@@ -52,4 +67,5 @@ newQuoteBtn.addEventListener("click", newQuote);
 twitterBtn.addEventListener("click", tweetQuote);
 
 //On load
+
 getQuotes();
