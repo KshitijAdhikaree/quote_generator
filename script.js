81078bc15036c9608f630d076af1eb905f498fc5
Kshitij adhikaree <adhikareekhsitij@gmail.com>
2023-02-27 18:23:02 +0545

    clickable link while clicking on button

--- script.js
+++ script.js
@@ -9,25 +9,27 @@ let apiQuotes = [];
 //Show New Quote
 function newQuote() {
   //Pick a random quote from apiQuotes array
+
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   //Check if Author field is blank and replace it with Unknown
-  if(!quote.author){
+
+  if (!quote.author) {
     authorText.textContent = "Unknown";
-  } else{
+  } else {
     authorText.textContent = quote.author;
   }
+
   //Check Quote length to determine styling
-  if(quote.text.length > 50){
-    quoteText.classList.add('long-quote')
-  } else{
-    quoteText.classList.remove('long-quote')
-
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
   }
-  
-  quoteText.textContent = quote.text
-}
-// Get Quotes form API
 
+  quoteText.textContent = quote.text;
+}
+
+// Get Quotes form API
 async function getQuotes() {
   const apiUrl = "https://type.fit/api/quotes";
   try {
@@ -38,6 +40,16 @@ async function getQuotes() {
     //Catch Error Here
   }
 }
+
+//Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_black");
+}
+
+//Event Listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
 
 //On load
 getQuotes();
